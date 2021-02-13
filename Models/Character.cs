040f5c98c6a56187b6ffb0045e0c886f06289041
Spaghetti-bit <yumyumkillkill@gmail.com>
2021-02-12 18:34:17 -0700

    Initial Character model, nothing special as of yet.

---
+++ Models/Character.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Character
+{
+	public Character()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
