688fabc4b249c821620e11726645094b0551a1f6
Spaghetti-bit <yumyumkillkill@gmail.com>
2021-02-16 18:43:55 -0700

    Spell.

---
+++ Models/Spell.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RpgInfinity.Models
+{
+    public abstract class Spell
+    {
+        // Level of Spell
+        public abstract int Level { get; protected set; }
+
+        // Spell's Name
+        public abstract string Name { get; protected set; }
+
+        // Spell Description, what it does.
+        public abstract string Effect { get; protected set; }
+    }
+}
