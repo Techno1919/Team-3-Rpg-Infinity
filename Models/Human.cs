0603472a9b3daad2c704f0f5f85b30eb81497752
Spaghetti-bit <yumyumkillkill@gmail.com>
2021-02-16 18:21:16 -0700

    CharacterRace fleshed out. 6 main abilities. Properties.
    Elf, Dwarf, and Human

---
+++ Models/Human.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RpgInfinity.Models
+{
+    public class Human : CharacterRace
+    {
+        public Human()
+        {
+            StrengthBonus = 1;
+            DexterityBonus = 1;
+            ConstituionBonus = 1;
+            IntelligenceBonus = 1;
+            WisdomBonus = 1;
+            CharismaBonus = 1;
+        }
+    }
+}
