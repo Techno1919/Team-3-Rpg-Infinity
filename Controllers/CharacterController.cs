d1c1d2a658f73f65853a64d72fc6d248603fd59f
Techno1919 <61212536+Techno1919@users.noreply.github.com>
2021-02-17 18:50:37 -0700

    Made the character controller and made the index

---
+++ Controllers/CharacterController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RpgInfinity.Controllers
+{
+    public class CharacterController : Controller
+    {
+        // GET: Character
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Character/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Character/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Character/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Character/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Character/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Character/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Character/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
