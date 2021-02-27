91f9155f920b2e205a3c2da8ac724a6e79d5ba2f
Garrick Kilpack <garrickkilpack@hotmail.com>
2021-02-26 18:46:04 -0700

    Added User model, made Login/Signup functionality, but not fully implemented.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using RpgInfinity.Models.Repos;
+using RpgInfinity.Models;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,6 +10,8 @@ namespace RpgInfinity.Controllers
 {
     public class HomeController : Controller
     {
+        public static User _currentUser = null;
+
         public ActionResult Index()
         {
             return View();
@@ -25,6 +29,67 @@ namespace RpgInfinity.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult Login()
+        {
+            ViewBag.Message = "Login Page";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(User user)
+        {
+            try
+            {
+                var repo = new CharacterRepo();
+
+                repo.LoginUser(user);
+
+                _currentUser = user;
+
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+                return View();
+            }
+        }
+
+        public ActionResult SignUp()
+        {
+            ViewBag.Message = "Create Account Page";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignUp(User user)
+        {
+            try
+            {
+                var repo = new CharacterRepo();
+
+                repo.AddUser(user);
+
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+                return View();
+            }
+        }
+
+        public ActionResult Logout()
+        {
+            _currentUser = null;
+
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace RpgInfinity.Models
+{
+    [Table("Users")]
+    public class User
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "Username is Required")]
+        [MinLength(4, ErrorMessage = "Username must be 4 characters or longer.")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password is Required")]
+        [MinLength(7, ErrorMessage = "Password must be 7 characters or longer.")]
+        public string Password { get; set; }
+    }
+}
